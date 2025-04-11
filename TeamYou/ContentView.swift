74c0374d0da024433447bf9559485462019e2d8e
ohcuy <everywavyday@gmail.com>
2025-04-11 16:18:45 +0900

    Initial Commit

---
+++ TeamYou/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TeamYou
+//
+//  Created by 조유진 on 4/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TeamYou/TeamYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamYouApp.swift
+//  TeamYou
+//
+//  Created by 조유진 on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TeamYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
