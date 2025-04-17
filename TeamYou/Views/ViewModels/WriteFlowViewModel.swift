773c861c7ddfb8f9a2863f4375420d34040b0a6f
ohcuy <everywavyday@gmail.com>
2025-04-17 10:59:38 +0900

    refactor: WriteFlowViewModel에서 불필요한 데이터 상태 제거

--- TeamYou/Views/ViewModels/WriteFlowViewModel.swift
+++ TeamYou/Views/ViewModels/WriteFlowViewModel.swift
@@ -4,14 +4,6 @@ import Combine
 class WriteFlowViewModel: ObservableObject {
     @Published var isPresented: Bool = false
 
-    @Published var selectedDate = Date()
-    @Published var selectedSituation = ""
-    @Published var selectedEmotion = ""
-    
-    @Published var content1 = ""
-    @Published var content2 = ""
-    @Published var content3 = ""
-    
     func closeFlow() {
         isPresented = false
     }
