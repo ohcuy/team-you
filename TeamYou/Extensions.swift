0f85535d8bedcd3606b8c8a5f9702cafb19daf61
ohcuy <everywavyday@gmail.com>
2025-04-21 14:08:32 +0900

    feat: 기록 공유 이미지를 클립보드에 복사하는 기능 구현

--- TeamYou/Extensions.swift
+++ TeamYou/Extensions.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SwiftUI
 
 extension Date {
     var year: Int {
@@ -11,5 +12,26 @@ extension Date {
     
     var day: Int {
         Calendar.current.component(.day, from: self)
+    }
+}
+
+extension View {
+    func snapshot(size: CGSize) -> UIImage {
+        let controller = UIHostingController(rootView: self
+                                                .frame(width: size.width, height: size.height))
+        let view = controller.view
+
+        let window = UIWindow(frame: CGRect(origin: .zero, size: size))
+        window.rootViewController = controller
+        window.makeKeyAndVisible()
+
+        view?.bounds = window.bounds
+        view?.backgroundColor = .clear
+        view?.layoutIfNeeded()
+
+        let renderer = UIGraphicsImageRenderer(size: size)
+        return renderer.image { _ in
+            view?.drawHierarchy(in: view!.bounds, afterScreenUpdates: true)
+        }
     }
 }
